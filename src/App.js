430dc937229f376c43cb24e21e198459eadbe278
Shivanand <Shivanand0297@gmail.com>
2023-01-22 12:13:21 +0530

    setup the routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Navbar from './components/Navbar'
+import About from './pages/About'
+import Home from './pages/Home'
+import "bootstrap/dist/css/bootstrap.min.css"
+import NoteState from './context/notes/NoteState'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
-  );
+    <NoteState>
+      <BrowserRouter>
+          <Navbar/>
+          <div className='container'>
+          <Routes>
+              <Route path='/' element={<Home/>} />
+              <Route path='/about' element={<About/>} />
+          </Routes>
+          </div>
+      </BrowserRouter>
+    </NoteState>
+  )
 }
 
-export default App;
+export default App
