14b131e762d53f3c18d298ce13005100c83a959a
Shivanand <Shivanand0297@gmail.com>
2023-01-23 19:13:22 +0530

    added signup and signin component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from './components/Navbar'
 import About from './pages/About'
 import Home from './pages/Home'
-import "bootstrap/dist/css/bootstrap.min.css"
 import NoteState from './context/notes/NoteState'
 import Alert from './components/Alert'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+import Login from './components/Login'
+import Signup from './components/Signup'
 
 const App = () => {
   return (
@@ -17,6 +20,8 @@ const App = () => {
           <Routes>
               <Route path='/' element={<Home/>} />
               <Route path='/about' element={<About/>} />
+              <Route path='/login' element={<Login/>} />
+              <Route path='/signup' element={<Signup/>} />
           </Routes>
           </div>
       </BrowserRouter>
