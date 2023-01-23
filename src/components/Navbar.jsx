89cd254b926527473a545313efbb630ae9fb284f
Shivanand <Shivanand0297@gmail.com>
2023-01-23 19:14:37 +0530

    added login and signup button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
-  <div className="collapse navbar-collapse" id="navbarNav">
+  <div className="collapse navbar-collapse w-full" id="navbarNav">
     <ul className="navbar-nav">
       <li className="nav-item">
         <Link className={`nav-link ${location.pathname === "/" ? "active" : "" }`} to="/">Home</Link>
@@ -19,6 +19,11 @@ const Navbar = () => {
         <Link className={`nav-link ${location.pathname === "/about" ? "active" : "" }`} to="/about">About</Link>
       </li>
     </ul>
+    <div className='float-end'>
+    <Link className="btn btn-primary mx-2" to={"/login"} role="button">Login</Link>
+    <Link className="btn btn-primary" to={"/signup"} role="button">Signup</Link>
+
+    </div>
   </div>
 </nav>
   )
