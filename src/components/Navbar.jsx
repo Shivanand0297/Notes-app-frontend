b8f726414f5780c21801db715535dd0d025865f1
Shivanand <Shivanand0297@gmail.com>
2023-01-23 21:50:09 +0530

    worked on condional rendering

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,80 @@
-import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import React from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  let location  = useLocation()
-  
+  let location = useLocation();
+  let navigate = useNavigate()
+  const handleLogout = () =>{
+    localStorage.clear()
+    navigate("/")
+  }
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <Link className="navbar-brand" to="/">Notes App</Link>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse w-full" id="navbarNav">
-    <ul className="navbar-nav">
-      <li className="nav-item">
-        <Link className={`nav-link ${location.pathname === "/" ? "active" : "" }`} to="/">Home</Link>
-      </li>
-      <li className="nav-item">
-        <Link className={`nav-link ${location.pathname === "/about" ? "active" : "" }`} to="/about">About</Link>
-      </li>
-    </ul>
-    <div className='float-end'>
-    <Link className="btn btn-primary mx-2" to={"/login"} role="button">Login</Link>
-    <Link className="btn btn-primary" to={"/signup"} role="button">Signup</Link>
+      <Link className="navbar-brand" to="/">
+        Notes App
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse w-full" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              className={`nav-link ${
+                location.pathname === "/" ? "active" : ""
+              }`}
+              to="/"
+            >
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              className={`nav-link ${
+                location.pathname === "/about" ? "active" : ""
+              }`}
+              to="/about"
+            >
+              About
+            </Link>
+          </li>
+        </ul>
+        <div className="float-end">
+          {/* if logged in then show logout button */}
+          {localStorage.getItem("token") ? (
+            <button
+              className="btn btn-primary mx-2"
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+          ) : (
+            <>
+              <Link
+                className="btn btn-primary mx-2"
+                to={"/login"}
+                role="button"
+              >
+                Login
+              </Link>
+              <Link className="btn btn-primary" to={"/signup"} role="button">
+                Signup
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-    </div>
-  </div>
-</nav>
-  )
-}
-
-export default Navbar
+export default Navbar;
