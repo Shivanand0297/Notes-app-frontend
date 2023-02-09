463f1d6640d216d63c1c82bd6995c23fd574f3ee
Shivanand <Shivanand0297@gmail.com>
2023-02-09 16:27:47 +0530

    backend url updated

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,1 +1,2 @@
-export const HOST = "http://ac-unj6hky-shard-00-00.pd9dy7d.mongodb.net:4000"
+// export const HOST = "http://ac-unj6hky-shard-00-00.pd9dy7d.mongodb.net:4000"
+export const HOST = "https://notes-app-production-eda9.up.railway.app"
